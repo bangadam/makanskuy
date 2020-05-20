dd5cc0847f440826ff16479cc1e371e1b6315f7b
bangadam <bangadam.dev@gmail.com>
2020-05-20 20:14:40 +0700

    delete unused package

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const flash = require("express-flash");
 const path = require("path");
 const mongoose = require("mongoose");
 const passport = require("passport");
-const expressStatusMonitor = require("express-status-monitor");
 const sass = require("node-sass-middleware");
 
 /**
@@ -65,7 +64,6 @@ app.set("host", process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0");
 app.set("port", process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(expressStatusMonitor());
 app.use(compression());
 app.use(
   sass({
@@ -135,7 +133,6 @@ app.post("/login", userController.postLogin);
 app.get("/logout", userController.logout);
 app.get("/register", userController.getSignup);
 app.post("/register", userController.postSignup);
-app.get("/account", passportConfig.isAuthenticated, userController.getAccount);
 app.get("/recipes", recipesController.index);
 app.get("/recipes/search", recipesController.search);
 app.post("/recipes/addToFavorite", recipesController.addToFavorite);
